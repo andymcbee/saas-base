8a3c015bae788d94cb6545be0ff6f83c887981df
Andrew <armcburn@gmail.com>
2024-10-23 14:40:51 -0400

    add actions column to data table

--- client/src/app/(dashboard)/dashboard/tasks/columns.tsx
+++ client/src/app/(dashboard)/dashboard/tasks/columns.tsx
@@ -1,7 +1,19 @@
 "use client";
 import { Task } from "./types";
 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { ColumnDef } from "@tanstack/react-table";
+
+import { MoreHorizontal } from "lucide-react";
 
 export const columns: ColumnDef<Task>[] = [
   {
@@ -19,6 +31,34 @@ export const columns: ColumnDef<Task>[] = [
       const date = new Date(row.getValue("createdAt"));
       const formatted = date.toLocaleDateString();
       return <div className="font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(payment.text.toString())
+              }
+            >
+              Copy Text
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
